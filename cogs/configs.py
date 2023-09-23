0d9468f3283a58d1397210d4953ede31de8c47b8
Earthman <960218mark@gmail.com>
2023-09-23 23:30:50 +0800

    Add permission checking for config commands
    - Add cog_check for Configs class (config.py)
    - Add `addmodrole` command to give roles access to config commands

--- cogs/configs.py
+++ cogs/configs.py
@@ -6,20 +6,28 @@ from discord.ext import commands
 DEFAULT_PREFIX = '$'
 
 
-class Configs(commands.Cog, name='Config Commands'):
+class Configs(commands.Cog, name = 'Config Commands'):
     def __init__(self, client) -> None:
         super().__init__()
         self.client = client
 
-    # TODO: check if user has admin perms / is a mod
     def cog_check(self, ctx) -> bool:
-        return True
+        with open('/app/data/mod_roles.json', 'r', encoding = 'UTF-8') as infile:
+            mod_roles = json.load(infile)
+
+            # get the intersection of mod_roles and ctx.author.roles
+            if ctx.guild.id in mod_roles:
+                intersection = [role for role in ctx.author.roles if str(
+                    role.id) in mod_roles[str(ctx.guild.id)]]
+            else:
+                intersection = []
+            return len(intersection) > 0 or ctx.author.guild_permissions.administrator
 
     @commands.hybrid_command(name='prefix')
     async def change_prefix(self, ctx, new_prefix):
         response_message = ''
         try:
-            with open('/app/data/prefixes.json', 'r+', encoding='UTF-8') as file:
+            with open('/app/data/prefixes.json', 'r+', encoding = 'UTF-8') as file:
                 prefixes = json.load(file)
                 prefixes[str(ctx.guild.id)] = new_prefix
 
@@ -36,6 +44,41 @@ class Configs(commands.Cog, name='Config Commands'):
             await ctx.interaction.response.send_message(response_message)
         else:
             await ctx.send(response_message)
+
+    @commands.hybrid_command(name='addmodrole')
+    async def add_mod_role(self, ctx):
+        selection = discord.ui.RoleSelect()
+        view = discord.ui.View()
+
+        async def callback(interaction):
+            try:
+                with open('/app/data/mod_roles.json', 'r+', encoding = 'UTF-8') as file:
+                    role_list = json.load(file)
+
+                    # create list if it doesn't exist
+                    if str(interaction.guild_id) not in role_list:
+                        role_list[str(interaction.guild_id)] = []
+
+                    if str(selection.values[0].id) not in role_list[str(interaction.guild_id)]:
+                        role_list[str(interaction.guild_id)].append(str(selection.values[0].id))
+                        await interaction.response.send_message(f'Successfully added {selection.values[0].name} to mod roles')
+                    else:
+                        await interaction.response.send_message(f'{selection.values[0].name} already added')
+
+                    # overwrite file with new data
+                    file.seek(0)
+                    file.truncate()
+                    json.dump(role_list, file)
+            except IOError:
+                await interaction.response.send_message('Exception when updating mod roles')
+
+        selection.callback = callback
+        view.add_item(selection)
+
+        if ctx.interaction is not None:
+            await ctx.interaction.response.send_message('Choose a role to add to the list of mod roles', view = view)
+        else:
+            await ctx.send('Choose a role to add to the list of mod roles', view = view)
 
 
 async def setup(client):
